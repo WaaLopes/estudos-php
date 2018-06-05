81c309e5ed666cdc73e804d81e2a8d5f2643dde1
Wagner Lopes Menezes Filho <wagnerl@lapisraro.com.br>
2018-06-05 11:42:43 -0300

    Laracasts lesson 15 PHP

--- pdo/bootstrap.php
+++ pdo/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
+$config = require 'config.php';
 require 'database/Connection.php';
 require 'database/QueryBuilder.php';
 require 'functions.php';
 
 return $query = new QueryBuilder(
-    Connection::make()
+    Connection::make($config['database'])
 );
